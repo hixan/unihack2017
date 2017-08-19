d0c4cf1378950e292919b4c8e350dae0c4a15b2d
Alex Elias <alex.88.elias@gmail.com>
2017-08-19 19:24:37 +1000

    is broken

--- Main/Recognition/uploadimage.py
+++ Main/Recognition/uploadimage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-import requests, json, base64, re, os
+import requests, json, base64, re, os, threading, datetime
 from requests.auth import HTTPBasicAuth
 from pathlib import PurePath
 
 print('test')
-def get_tags(image_filename):
+def get_tags(image_filename, service='tagging3'):
     url = 'http://smartvision.aiam-dh.com:8080/api/v1.0'
     authorization = HTTPBasicAuth('demo3','hackathon6483')
 
@@ -17,10 +17,10 @@ def get_tags(image_filename):
     r_upload = requests.post(
         url+'/tasks',
         auth=authorization,
-        data=json.dumps({'service':'tagging3', 'image':str(payload)[2:-1]}),
+        data=json.dumps({'service':service, 'image':str(payload)[2:-1]}),
         headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     )
-    #print(r_upload, r_upload.text)
+    print(r_upload, r_upload.text)
 
 
     #find the ID of this image
@@ -33,8 +33,8 @@ def get_tags(image_filename):
         headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'},
         data=json.dumps({'scanned':True})
     )
-    #print(r_run, r_run.text)
-    #print(im_id)
+    print(r_run, r_run.text)
+    print(im_id)
 
     # get the information from the analysis
     r_complete = requests.get(
@@ -42,7 +42,7 @@ def get_tags(image_filename):
         auth=authorization,
         headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'},
     )
-    #print(r_complete, r_complete.text)
+    print(r_complete, r_complete.text)
 
     # delete the image on the server
     r_delete = requests.delete(
@@ -50,12 +50,26 @@ def get_tags(image_filename):
         auth=authorization,
         headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     )
-    #print(r_delete, r_delete.text, im_id)
+    print(r_delete, r_delete.text, im_id)
 
     # find the required tags from the analysis
-    return zip(r_run.json()['task']['description'], r_run.json()['task']['confidence'])
+    return zip(r_run.json()['task']['description'], r_run.json()['task']['confidence'], r_run.text)
 
-for item in os.listdir(PurePath(os.path.realpath(__name__), '..', 'sample images')):
-    print('\n'+item)
-    for tag in get_tags(item):
-        print('\t', *tag)
+if __name__ == '__main__':
+    service = 'tagging3'
+    with open('output-'+service+'.txt', 'w') as resultsfile:
+        threadlock = threading.Lock()
+        def print_tags(image_file):
+            tags = get_tags(item)
+            print('locking thread for {}'.format(item))
+            threadlock.acquire()
+            print('\n'+image_file, file=resultsfile)
+            for tag in tags:
+                print('\t', *tag, file=resultsfile)
+            threadlock.release()
+            print('releasing thread for {}'.format(item))
+        for item in os.listdir(PurePath(os.path.realpath(__name__), '..', 'sample images')):
+            print('starting thread at {}'.format(datetime.datetime.time(datetime.datetime.now())), file=resultsfile)
+            newthread = threading.Thread(target=print_tags,args=(item,))
+            newthread.start()
+            
