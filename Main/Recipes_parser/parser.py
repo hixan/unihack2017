0e2e584c45723c7274db843b74bf3fd1064e248f
Alex Elias <alex.88.elias@gmail.com>
2017-08-19 23:17:11 +1000

    initial parser skeleton

--- Main/Recipes_parser/parser.py
+++ Main/Recipes_parser/parser.py
@@ -1,29 +1,10 @@
-def parser(values):
-  results = []
-  results2 = []
-  results3 = []
-  results4 = []
-  name = ""
-  with open('../../Tests/basic_test_data.txt') as inputfile:
-    for line in inputfile:
-      results.append(line.strip().strip(","))
-  for line in results:
-    if(len(line) > 0):
-      if((line[0] == '"')and(line.find(":")==-1)):
-        results2.append(line)
-      if(line.find("title") != -1):
-        if(len(results2) != 0):
-          results4 = []
-          results4.append(name)
-          results4.append(results2)
-          results3.append(results4)
-          results2 = []
-        name = line
-  for line in results3:
-    for item in line[1]:
-      for value in values:
-        if value not in item:
-          results2.append(item)
-      print(line[0])
-  print(values)
-parser('milk')
+#!/usr/bin/python3
+
+import json, pathlib
+
+if __name__ == '__main__':
+    path = (pathlib.Path(__file__).parents[2].joinpath('Tests', 'basic_test_data.txt'))
+    print(path)
+    with open(path, encoding='utf8') as json_file:
+        database = json.load(json_file)
+    print(database)
