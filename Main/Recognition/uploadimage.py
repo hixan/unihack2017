6fdc2c1cb3d19c542cdf490d4e7dba9c687e4468
Alex Elias <alex.88.elias@gmail.com>
2017-08-19 20:47:59 +1000

    started work on threading subclass

--- Main/Recognition/uploadimage.py
+++ Main/Recognition/uploadimage.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 
-import requests, json, base64, re, os, threading, datetime
+import requests, json, base64, re, os, threading, datetime, io
 from requests.auth import HTTPBasicAuth
 from pathlib import PurePath
+from PIL import Image
 
-print('test')
-def get_tags(image_filename, service='tagging3'):
-    url = 'http://smartvision.aiam-dh.com:8080/api/v1.0'
-    authorization = HTTPBasicAuth('demo3','hackathon6483')
-
-    # upload an image through the API:
-
+def encode_image_from_file(image_filename):
     image = 'sample images/'+image_filename
     with open(image, 'rb') as image_file:
         payload = base64.b64encode(image_file.read())
+    return payload
+
+def get_tags(encoded_image, service, logfile=None):
+    url = 'http://smartvision.aiam-dh.com:8080/api/v1.0'
+    authorization = HTTPBasicAuth('demo3','hackathon6483')
+
+    # upload an {}m.format( through the API:)
+    payload = encoded_image
     r_upload = requests.post(
         url+'/tasks',
         auth=authorization,
         data=json.dumps({'service':service, 'image':str(payload)[2:-1]}),
         headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     )
-    print(r_upload, r_upload.text)
+    print(r_upload, r_upload.text, file=logfile)
 
 
     #find the ID of this image
@@ -33,7 +36,7 @@ def get_tags(image_filename, service='tagging3'):
         headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'},
         data=json.dumps({'scanned':True})
     )
-    print(r_run, r_run.text)
+    print(r_run, r_run.text, file=logfile)
     print(im_id)
 
     # get the information from the analysis
@@ -42,7 +45,7 @@ def get_tags(image_filename, service='tagging3'):
         auth=authorization,
         headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'},
     )
-    print(r_complete, r_complete.text)
+    print(r_complete, r_complete.text, file=logfile)
 
     # delete the image on the server
     r_delete = requests.delete(
@@ -50,26 +53,40 @@ def get_tags(image_filename, service='tagging3'):
         auth=authorization,
         headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     )
-    print(r_delete, r_delete.text, im_id)
+    print(r_delete, r_delete.text, im_id, file=logfile)
 
     # find the required tags from the analysis
-    return zip(r_run.json()['task']['description'], r_run.json()['task']['confidence'], r_run.text)
+    return r_run.json()['task']
 
+class tag_retriever(threading.Thread):
+    def __init__(self, image_byte_array, method, return_keywords):
+        self.image_file = base64.b64encode(image_byte_array)
+        self.method = method
+        self.returnkw = return_keywords
+    def run(self, logfile=None):
+        url = 'http://smartvision.aiam-dh.com:8080/api/v1.0/tasks'
+        authorization = HTTPBasicAuth('demo3', 'hackathon6483')
+    def upload(self, url, auth):
+        self.r_upload = requests.post(
+                url,
+                auth=auth,
+                data=json.dumps({'service':self.method, 'image':str(self.image_file)[2:-1]}),
+                headers={'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+        )
+        
 if __name__ == '__main__':
-    service = 'tagging3'
-    with open('output-'+service+'.txt', 'w') as resultsfile:
-        threadlock = threading.Lock()
-        def print_tags(image_file):
-            tags = get_tags(item)
-            print('locking thread for {}'.format(item))
-            threadlock.acquire()
-            print('\n'+image_file, file=resultsfile)
-            for tag in tags:
-                print('\t', *tag, file=resultsfile)
-            threadlock.release()
-            print('releasing thread for {}'.format(item))
-        for item in os.listdir(PurePath(os.path.realpath(__name__), '..', 'sample images')):
-            print('starting thread at {}'.format(datetime.datetime.time(datetime.datetime.now())), file=resultsfile)
-            newthread = threading.Thread(target=print_tags,args=(item,))
-            newthread.start()
-            
+    image = 'fridge contents.jpg'
+    with Image.open('sample images/'+image) as im:
+        im_byte_arr = io.BytesIO()
+        im.save(im_byte_arr, format='JPEG')
+        im_byte_arr = im_byte_arr.getvalue()
+        im_byte_arr = base64.b64encode(im_byte_arr)
+        result = get_tags(im_byte_arr, 'tagging3')['description']
+        print(result)
+        for tag in result:
+            im.crop(*list(map(lambda x : x[1], re.findall(r'(Left|Top|Right|Bottom)=(\d+)', tag))))
+
+
+
+
+
